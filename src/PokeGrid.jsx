e918a0561cd9aeaf2cc1e6ff9fdcac1900e912b2
laurajardim077 <rm94797@estudante.fieb.edu.br>
2025-05-21 11:23:01 +0000

    grid

---
+++ src/PokeGrid.jsx
@@ -0,0 +1,30 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './PokeGrid.css'
+
+
+const PokeGrid = () => {
+
+    return (
+        <div>
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+
+
+
+
+        </div>
+    )}
+    
+export default PokeGrid
+
+
+ 
